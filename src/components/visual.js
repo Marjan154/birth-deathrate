df8af0f558922b253a2d9417bd726cb8274e0ce1
yitongni <31358391+yitongni@users.noreply.github.com>
2019-11-29 18:44:23 -0500

    Added more graphs

--- src/components/visual.js
+++ src/components/visual.js
@@ -13,7 +13,7 @@ class Visual extends Component {
       }  
 
       initViz() {  
-        var url = "https://public.tableau.com/views/BirthRateByYear/BirthRatebyYear?:display_count=y&publish=yes&:origin=viz_share_link";
+        var url = "https://public.tableau.com/views/BirthDeath/Sheet8?:display_count=y&publish=yes&:origin=viz_share_link";
         const vizContainer = this.vizContainer;  
             var options = {
             hideTabs: true,
@@ -25,9 +25,9 @@ class Visual extends Component {
         };
         let viz = new window.tableau.Viz(vizContainer, url, options)  
 
-        var url2 = "https://public.tableau.com/views/PopulationByYear/Sheet2?:display_count=y&publish=yes&:origin=viz_share_link";
-        const vizContainer2 = this.vizContainer2;  
-            var options2 = {
+        var populationByYearUrl = "https://public.tableau.com/shared/YG9BWBTQW?:display_count=y&:origin=viz_share_link";
+        const populationByYearvizContainer = this.populationByYearvizContainer;  
+            var populationByYearOptions = {
             hideTabs: true,
             width: "800px",
             height: "700px",
@@ -35,32 +35,43 @@ class Visual extends Component {
      // The viz is now ready and can be safely used.
             }
         };
-        let viz2 = new window.tableau.Viz(vizContainer2, url2, options2)  
+        let populationByYearViz = new window.tableau.Viz(populationByYearvizContainer, populationByYearUrl, populationByYearOptions)  
 
-        var url3 = "https://public.tableau.com/views/BirthDeath/BirthRateofeachCountry?:display_count=y&publish=yes&:origin=viz_share_link";
-        const vizContainer3 = this.vizContainer3;  
-            var options3 = {
-            hideTabs: true,
-            width: "800px",
-            height: "700px",
-            onFirstInteractive: function() {
-     // The viz is now ready and can be safely used.
-            }
-        };
-        let viz3 = new window.tableau.Viz(vizContainer3, url3, options3) 
+    //     var url3 = "https://public.tableau.com/views/BirthDeath/BirthRateofeachCountry?:display_count=y&publish=yes&:origin=viz_share_link";
+    //     const vizContainer3 = this.vizContainer3;  
+    //         var options3 = {
+    //         hideTabs: true,
+    //         width: "800px",
+    //         height: "700px",
+    //         onFirstInteractive: function() {
+    //  // The viz is now ready and can be safely used.
+    //         }
+    //     };
+    //     let viz3 = new window.tableau.Viz(vizContainer3, url3, options3) 
 
-        var url4 = "https://public.tableau.com/views/BirthDeath/Sheet4?:display_count=y&publish=yes&:origin=viz_share_link";
-        const vizContainer4 = this.vizContainer4;  
-            var options4 = {
-            hideTabs: true,
-            width: "800px",
-            height: "700px",
-            onFirstInteractive: function() {
-     // The viz is now ready and can be safely used.
-            }
-        };
-        let viz4 = new window.tableau.Viz(vizContainer4, url4, options4) 
+    //     var url4 = "https://public.tableau.com/views/BirthDeath/Sheet4?:display_count=y&publish=yes&:origin=viz_share_link";
+    //     const vizContainer4 = this.vizContainer4;  
+    //         var options4 = {
+    //         hideTabs: true,
+    //         width: "800px",
+    //         height: "700px",
+    //         onFirstInteractive: function() {
+    //  // The viz is now ready and can be safely used.
+    //         }
+    //     };
+    //     let viz4 = new window.tableau.Viz(vizContainer4, url4, options4) 
         
+        var url5 = "https://public.tableau.com/shared/Q85R52WDD?:display_count=y&:origin=viz_share_link";
+        const vizContainer5 = this.vizContainer5;  
+            var options5 = {
+            hideTabs: true,
+            width: "800px",
+            height: "700px",
+            onFirstInteractive: function() {
+     // The viz is now ready and can be safely used.
+            }
+        };
+        let viz5 = new window.tableau.Viz(vizContainer5, url5, options5) 
         
     
       }  
@@ -73,9 +84,12 @@ class Visual extends Component {
         
         return(
           <div>
-            <div ref={(div) => { this.vizContainer = div }}> </div>  
-            <div ref={(div) => { this.vizContainer2 = div }}> </div> 
+            { <div ref={(div) => { this.vizContainer = div }}> </div>  
+            /*<div ref={(div) => { this.vizContainer2 = div }}> </div> 
             <div ref={(div) => { this.vizContainer3 = div }}> </div> 
+            <div ref={(div) => { this.vizContainer4 = div }}> </div>  */}
+            <div ref={(div) => { this.populationByYearViz = div }}> </div> 
+            <div ref={(div) => { this.vizContainer5 = div }}> </div> 
           </div>
         );
     }
