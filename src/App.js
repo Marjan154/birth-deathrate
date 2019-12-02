97ef60d0219d01e1216d0a99355ece317a6c87a7
MarjanGit <mansar16@huntersoe.org>
2019-12-01 19:00:50 -0500

    baby styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Visual from './components/visual.js';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Visual from "./components/visual.js";
 
 function App() {
   return (
     <div className="App">
+      <h1 id="title">Birth and Death Rates</h1>
+      <p className="content">
+        Birth and Death rates is crucial to asseccing humanitys' progress and
+        advancement. If you look at our visualizations, you might find
+        interesting trends or unexpected information. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+        qui officia deserunt mollit anim id est laborum.
+      </p>
       <Visual></Visual>
     </div>
   );
--- src/components/visual.js
+++ src/components/visual.js
@@ -1,97 +1,94 @@
 import React, { Component } from "react";
-import tableau from 'tableau-api';
+import tableau from "tableau-api";
+import styles from "../styles/home.css";
 
 class Visual extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    componentDidMount() {  
-        this.initViz()  
-      }  
+  componentDidMount() {
+    this.initViz();
+  }
 
-      initViz() {  
-        var url = "https://public.tableau.com/views/BirthDeath/Sheet8?:display_count=y&publish=yes&:origin=viz_share_link";
-        const vizContainer = this.vizContainer;  
-            var options = {
-            hideTabs: true,
-            width: "800px",
-            height: "700px",
-            onFirstInteractive: function() {
-     // The viz is now ready and can be safely used.
-            }
-        };
-        let viz = new window.tableau.Viz(vizContainer, url, options)  
+  initViz() {
+    const url =
+      "https://public.tableau.com/views/BirthDeath/Sheet8?:display_count=y&publish=yes&:origin=viz_share_link";
+    const vizContainer = this.vizContainer;
+    const options = {
+      hideTabs: true,
+      width: "800px",
+      height: "700px",
+      onFirstInteractive: function() {
+        // The viz is now ready and can be safely used.
+      }
+    };
+    let viz = new window.tableau.Viz(vizContainer, url, options);
 
-        var populationByYearUrl = "https://public.tableau.com/shared/YG9BWBTQW?:display_count=y&:origin=viz_share_link";
-        const populationByYearvizContainer = this.populationByYearvizContainer;  
-            var populationByYearOptions = {
-            hideTabs: true,
-            width: "800px",
-            height: "700px",
-            onFirstInteractive: function() {
-     // The viz is now ready and can be safely used.
-            }
-        };
-        let populationByYearViz = new window.tableau.Viz(populationByYearvizContainer, populationByYearUrl, populationByYearOptions)  
+    const populationByYearUrl =
+      "https://public.tableau.com/shared/YG9BWBTQW?:display_count=y&:origin=viz_share_link";
+    const populationByYearvizContainer = this.populationByYearvizContainer;
+    const populationByYearOptions = {
+      hideTabs: true,
+      width: "800px",
+      height: "700px",
+      onFirstInteractive: function() {
+        // The viz is now ready and can be safely used.
+      }
+    };
+    let populationByYearViz = new window.tableau.Viz(
+      populationByYearvizContainer,
+      populationByYearUrl,
+      populationByYearOptions
+    );
 
-    //     var url3 = "https://public.tableau.com/views/BirthDeath/BirthRateofeachCountry?:display_count=y&publish=yes&:origin=viz_share_link";
-    //     const vizContainer3 = this.vizContainer3;  
-    //         var options3 = {
-    //         hideTabs: true,
-    //         width: "800px",
-    //         height: "700px",
-    //         onFirstInteractive: function() {
-    //  // The viz is now ready and can be safely used.
-    //         }
-    //     };
-    //     let viz3 = new window.tableau.Viz(vizContainer3, url3, options3) 
+    const url5 =
+      "https://public.tableau.com/shared/Q85R52WDD?:display_count=y&:origin=viz_share_link";
+    const vizContainer5 = this.vizContainer5;
+    const options5 = {
+      hideTabs: true,
+      width: "800px",
+      height: "700px",
+      onFirstInteractive: function() {
+        // The viz is now ready and can be safely used.
+      }
+    };
+    let viz5 = new window.tableau.Viz(vizContainer5, url5, options5);
+  }
 
-    //     var url4 = "https://public.tableau.com/views/BirthDeath/Sheet4?:display_count=y&publish=yes&:origin=viz_share_link";
-    //     const vizContainer4 = this.vizContainer4;  
-    //         var options4 = {
-    //         hideTabs: true,
-    //         width: "800px",
-    //         height: "700px",
-    //         onFirstInteractive: function() {
-    //  // The viz is now ready and can be safely used.
-    //         }
-    //     };
-    //     let viz4 = new window.tableau.Viz(vizContainer4, url4, options4) 
-        
-        var url5 = "https://public.tableau.com/shared/Q85R52WDD?:display_count=y&:origin=viz_share_link";
-        const vizContainer5 = this.vizContainer5;  
-            var options5 = {
-            hideTabs: true,
-            width: "800px",
-            height: "700px",
-            onFirstInteractive: function() {
-     // The viz is now ready and can be safely used.
-            }
-        };
-        let viz5 = new window.tableau.Viz(vizContainer5, url5, options5) 
-        
-    
-      }  
+  render() {
+    // const placeholderDiv = document.getElementById("tableauViz");
 
-    render() {
-        // var placeholderDiv = document.getElementById("tableauViz");
-        
-    
-    //     var viz = new tableau.Viz(placeholderDiv, url, options);   
-        
-        return(
-          <div>
-            { <div ref={(div) => { this.vizContainer = div }}> </div>  
-            /*<div ref={(div) => { this.vizContainer2 = div }}> </div> 
-            <div ref={(div) => { this.vizContainer3 = div }}> </div> 
-            <div ref={(div) => { this.vizContainer4 = div }}> </div>  */}
-            <div ref={(div) => { this.populationByYearViz = div }}> </div> 
-            <div ref={(div) => { this.vizContainer5 = div }}> </div> 
+    //     const viz = new tableau.Viz(placeholderDiv, url, options);
+
+    return (
+      <div className="viz-graphs">
+        {
+          <div
+            ref={div => {
+              this.vizContainer = div;
+            }}
+          >
+            {" "}
           </div>
-        );
-    }
+        }
+        <div
+          ref={div => {
+            this.populationByYearViz = div;
+          }}
+        >
+          {" "}
+        </div>
+        <div
+          ref={div => {
+            this.vizContainer5 = div;
+          }}
+        >
+          {" "}
+        </div>
+      </div>
+    );
+  }
 }
 export default Visual;
